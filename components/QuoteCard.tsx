e8729d53290bb592509aab478a0f75e7a2e9d4e2
devcer <viswanathamsantosh@gmail.com>
2020-09-18 14:05:40 +0530

    Integrated copy to clipboard module

--- components/QuoteCard.tsx
+++ components/QuoteCard.tsx
@@ -1,14 +1,14 @@
 import * as WebBrowser from 'expo-web-browser';
-import React from 'react';
-import { StyleSheet, TouchableOpacity, Button, Alert} from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, TouchableOpacity, Button, Alert, Clipboard} from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 
 import Colors from '../constants/Colors';
 import { View, Text } from './Themed';
 
-
 export default function QuoteCard(props: {quote: Quote}) {
     
+    const [copiedQuote, setCopiedQuote] = useState('');
 
     return(
         <View style={styles.container}>
@@ -19,22 +19,26 @@ export default function QuoteCard(props: {quote: Quote}) {
                 title="Copy Quote"
                 onPress={() => copyQuote()}
             /> */}
-            <FontAwesome.Button name="copy" backgroundColor="#000" onPress={() => copyQuote()}>
+            <FontAwesome.Button name="copy" backgroundColor="#000" onPress={copyToClipboard}>
                 Copy
             </FontAwesome.Button>
         </View>
     );
-    function copyQuote() {
-        alert("copyQuote");
+    function copyToClipboard(ev) {
+        Clipboard.setString(props.quote.quote);
     }
-    
+
+    const fetchCopiedText = async () => {
+        const text = await Clipboard.getString()
+        setCopiedQuote(text)
+    }
 }
 
 const styles = StyleSheet.create({
     container: {
         borderColor: "#cccccc",
         borderWidth: 2,
-        flex: 1
+        // flex: 1
     },
     moodText: {
         fontSize: 16,
